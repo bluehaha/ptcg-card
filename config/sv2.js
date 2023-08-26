20e8f5903052caa7fe68be0e2108cae00b6d22f7
bluehaha <laiyushing@gmail.com>
2023-08-26 10:37:10 +0800

    update list

---
+++ config/sv2.js
@@ -0,0 +1,40 @@
+export default [
+  // 151
+  { src: "https://i.imgur.com/QmcT2R1.jpg", price: 50 },
+  { src: "https://i.imgur.com/keCeZeX.jpg", price: 50 },
+  { src: "https://i.imgur.com/CbyoPiW.jpg", price: 50 },
+  { src: "https://i.imgur.com/tu6ctrd.jpg", price: 20 },
+  { src: "https://i.imgur.com/VsehJIp.jpg", price: 20 },
+  { src: "https://i.imgur.com/Ndr2Y27.jpg", price: 50 },
+  { src: "https://i.imgur.com/FRGkc8y.jpg", price: 40 },
+  // sr
+  { src: "https://i.imgur.com/Nu4ETZ3.jpg", price: 50 },
+  { src: "https://i.imgur.com/USl0cOc.jpg", price: 100 },
+  { src: "https://i.imgur.com/QK3WzSZ.jpg", price: 50 },
+  { src: "https://i.imgur.com/wfRC4os.jpg", price: 50 },
+  { src: "https://i.imgur.com/Ay5PU7m.jpg", price: 50 },
+  { src: "https://i.imgur.com/mPRhMPo.jpg", price: 50 },
+  { src: "https://i.imgur.com/YTp0iPG.jpg", price: 50 },
+  { src: "https://i.imgur.com/8ZBUEML.jpg", price: 200 },
+  // sar
+  // { src: "https://i.imgur.com/794aApl.jpg", price: 20 },
+  // { src: "https://i.imgur.com/vu60E8u.jpg", price: 20 },
+  // { src: "https://i.imgur.com/voaZWRe.jpg", price: 20 },
+  // { src: "https://i.imgur.com/qyBfhKm.jpg", price: 20 },
+  // { src: "https://i.imgur.com/Z0v7sdb.jpg", price: 20 },
+  { src: "https://i.imgur.com/f1FzoKd.jpg", price: 20 },
+  { src: "https://i.imgur.com/xCIYl0s.jpg", price: 50 },
+  // 古劍豹 sr
+  // { src: "https://i.imgur.com/f38RLkE.jpg", price: 100 },
+  // 怒鸚哥 sr
+  { src: "https://i.imgur.com/nC6VRxT.jpg", price: 50 },
+  { src: "https://i.imgur.com/slq8Pva.jpg", price: 150 },
+  // 古劍豹 sar
+  // { src: "https://i.imgur.com/9QjBM3o.jpg", price: 400 },
+  { src: "https://i.imgur.com/5mENPC2.jpg", price: 20 },
+  { src: "https://i.imgur.com/T1WRMYW.jpg", price: 20 },
+  { src: "https://i.imgur.com/qJEFWS9.jpg", price: 20 },
+  { src: "https://i.imgur.com/DREwbqQ.jpg", price: 150 },
+  { src: "https://i.imgur.com/ZNREDwu.jpg", price: 300 },
+  { src: "https://i.imgur.com/y58CvQu.jpg", price: 150 },
+];
